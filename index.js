90cca68efea683a5a16111871ff7bf4443e55e45
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-10 14:04:22 -0500

    starting npm app + getting corrected patient data to console

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 "use strict";
 const express = require("express");
+const patientData = require("./corrected-patient.json");
 const server = express();
+
+server.listen(3000, function() {
+    console.log("Server listening on localhost:3000");
+});
+
+server.get("/", (req, res) => {
+    console.log(patientData);
+});
