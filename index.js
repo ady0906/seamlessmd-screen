46ed9487557dabecac1369ba2aa9025f0579a81d
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-10 14:53:46 -0500

    requiring pug in index file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 "use strict";
-const express = require("express");
-const patientData = require("./corrected-patient.json");
-const server = express();
+const express       = require("express");
+const patientData   = require("./corrected-patient.json");
+const server        = express();
+const pug           = require("pug");
 
 server.listen(3000, function() {
     console.log("Server listening on localhost:3000");
 });
 
 server.get("/", (req, res) => {
-    console.log(patientData);
+    
 });
